ee6ed2f140bd7c42b693f2aba01eb9a88a8e5c65
Alfonso Lopez <lopezpanduro@chapman.edu>
2025-10-26 23:29:39 -0700

    animals.sh

--- animals.sh
+++ animals.sh
@@ -14,8 +14,8 @@ esac
 while [ "$animal" != "Goodbye" ]; do
   read -r -p "Enter an animal (in ALL CAPS): " animal
   case $animal in
-    DOG) echo "Woof! friendly companion" ;;
-    CAT) echo "Meow! your sassy independent and curious housekeeper." ;;
+    DOG) echo "Woof! your friendly companion" ;;
+    CAT) echo "Meow! your sassy, independent, and curious housekeeper." ;;
     *) echo "wild animal." ;;
   esac
 done
