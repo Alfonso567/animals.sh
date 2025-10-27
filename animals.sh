565532f0ad0c58effd3a5601fcf2b0923c40a566
Alfonso Lopez <lopezpanduro@chapman.edu>
2025-10-26 23:33:41 -0700

    animals.sh

--- animals.sh
+++ animals.sh
@@ -6,16 +6,18 @@
 read -r -p "Enter an animal (in ALL CAPS): " animal
 
 case $animal in
+  TIGER) echo "A WILD ANIMAL!" ;;
   DOG) echo "Woof! your loyal and friendly companion." ;;
   CAT) echo "Meow! your sassy independent and curious housekeeper." ;;
-  *) echo "wild animal." ;;
+  *) echo "unknown animal." ;;
 esac
 
 while [ "$animal" != "Goodbye" ]; do
   read -r -p "Enter an animal (in ALL CAPS): " animal
   case $animal in
-    DOG) echo "Woof! your friendly companion" ;;
+    TIGER) echo "A WILD ANIMAL!" ;;
+    DOG) echo "Woof! your loyal and friendly companion." ;;
     CAT) echo "Meow! your sassy, independent, and curious housekeeper." ;;
-    *) echo "wild animal." ;;
+    *) echo "unknown animal." ;;
   esac
 done
